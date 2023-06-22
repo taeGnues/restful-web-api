463e930f8fa54a14ede3c8a09449ff5bf705f190
정태승 <hi563@g.skku.edu>
2023-06-22 16:42:01 +0900

    Spring Boot - Error Message 출력하기

--- src/main/java/com/learnspringboot/restfulwebapi/webservice/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/learnspringboot/restfulwebapi/webservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,14 +1,18 @@
 package com.learnspringboot.restfulwebapi.webservice.exception;
 
 import com.learnspringboot.restfulwebapi.webservice.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @ControllerAdvice // 이 프로젝트에 정의된 모든 컨트롤러를 의미함.
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
@@ -27,4 +31,16 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 
         return new ResponseEntity(errorDetails, HttpStatus.NOT_FOUND);
     } // 예외를 리턴해줌.
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDate.now(), "Total Errors:" + ex.getErrorCount() + " First Error:" + ex.getFieldError().getDefaultMessage(), request.getDescription(false));
+        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+
 }
--- src/main/java/com/learnspringboot/restfulwebapi/webservice/user/User.java
+++ src/main/java/com/learnspringboot/restfulwebapi/webservice/user/User.java
@@ -1,10 +1,16 @@
 package com.learnspringboot.restfulwebapi.webservice.user;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public class User {
+
     private Integer id;
+    @Size(min=2, message = "Name Should 2") // 검증조건
     private String name;
+    @Past(message = "plz past")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
