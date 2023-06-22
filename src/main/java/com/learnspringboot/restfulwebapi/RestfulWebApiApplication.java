cfde4d3b2c48724d762b720af1ad4e0863830b24
정태승 <hi563@g.skku.edu>
2023-06-22 13:40:41 +0900

    Spring Boot - Settings RESTful API

---
+++ src/main/java/com/learnspringboot/restfulwebapi/RestfulWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.learnspringboot.restfulwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnspringboot/restfulwebapi/RestfulWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspringboot.restfulwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
