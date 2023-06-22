12201958bcd44283c30745e6934e47f840aace44
정태승 <hi563@g.skku.edu>
2023-06-22 15:33:00 +0900

    Spring Boot - Post 구현

--- src/main/java/com/learnspringboot/restfulwebapi/webservice/user/UserDAOService.java
+++ src/main/java/com/learnspringboot/restfulwebapi/webservice/user/UserDAOService.java
@@ -10,20 +10,23 @@ import java.util.function.Predicate;
 @Component
 public class UserDAOService {
     private static List<User> users = new ArrayList<>();
+    private static int usersCount = 0;
 
     static {
-        users.add(new User(1, "Adams", LocalDate.now().minusYears(30)));
-        users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
-        users.add(new User(3, "Jim", LocalDate.now().minusYears(20)));
+        users.add(new User(++usersCount, "Adams", LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount, "Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(++usersCount, "Jim", LocalDate.now().minusYears(20)));
 
     }
     public List<User> findAll(){
         return users;
     }
 
-//    public User save(User user){
-//
-//    }
+    public User save(User user){
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
+    }
 
     public User findOne(int id){
         Predicate<? super User> predicate = user -> user.getId().equals(id);
--- src/main/java/com/learnspringboot/restfulwebapi/webservice/user/UserResource.java
+++ src/main/java/com/learnspringboot/restfulwebapi/webservice/user/UserResource.java
@@ -1,9 +1,10 @@
 package com.learnspringboot.restfulwebapi.webservice.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -16,13 +17,23 @@ public class UserResource {
 
     //GET /users
     @GetMapping("/users")
-    public List<User> retrieveAllUsers(){
+    public List<User> retrieveAllUsers() {
         return service.findAll();
     }
 
     //GET /users/1
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
