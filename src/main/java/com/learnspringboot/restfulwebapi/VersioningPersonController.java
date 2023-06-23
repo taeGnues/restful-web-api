3796a43a018bf65f7f7816037082869878b7083e
정태승 <hi563@g.skku.edu>
2023-06-23 10:12:05 +0900

    Spring Boot - Accept, Accept-Language header 사용하기

---
+++ src/main/java/com/learnspringboot/restfulwebapi/VersioningPersonController.java
@@ -0,0 +1,4 @@
+package com.learnspringboot.restfulwebapi;
+
+public class VersioningPersonController {
+}
--- src/main/java/com/learnspringboot/restfulwebapi/helloworld/HelloWorldController.java
+++ src/main/java/com/learnspringboot/restfulwebapi/helloworld/HelloWorldController.java
@@ -1,20 +1,34 @@
 package com.learnspringboot.restfulwebapi.helloworld;
 
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 //REST API
 @RestController
 public class HelloWorldController {
-    //hello-world
 
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+    @GetMapping(path = "/hello-world-international")
+    public String helloWorldInternationalized(){
+
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "DefaultMessage", locale);
+    }
 
     //"Hello-World"
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
-        return "Hello World";
+        return "Hello Around of Studio";
     }
 
     @GetMapping(path = "/hello-world-bean")
@@ -28,5 +42,6 @@ public class HelloWorldController {
                 String.format("Hello World, %s", name)
         );
     }
+
 
 }
